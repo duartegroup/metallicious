83ffd952417c03bf2c934209cdb8e899c6dd7edb
tkpiskorz <tomasz.k.piskorz@gmail.com>
2024-06-05 15:28:58 +0100

    documentation update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 #html_static_path = ['_static']
-html_logo = "logo_square.png"
+html_logo = "logo_rect.png"
 html_theme_options = {
     'logo_only': False
 }
