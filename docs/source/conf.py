9939d0e279bfc6dd53fe57ef8c51e591ac34c9d2
tkpiskorz <tomasz.k.piskorz@gmail.com>
2024-06-05 15:20:15 +0100

    documentation update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,10 +31,7 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_logo = "logo.png"
-#html_theme_options = {
-#    'logo_only': True
-#}
+html_logo = "logo_square.png"
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
