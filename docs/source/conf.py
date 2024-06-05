05a7c8dc61a083b720d5a11247ecea4700ed47e1
tkpiskorz <tomasz.k.piskorz@gmail.com>
2024-06-05 15:05:00 +0100

    documentation update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,7 +4,7 @@
 
 project = 'metallicious'
 copyright = '2024, Piskorz'
-author = 'Piskorz'
+author = 'tkpiskorz'
 
 release = '0.1'
 version = '0.1.0'
@@ -30,6 +30,12 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_logo = "logo.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
